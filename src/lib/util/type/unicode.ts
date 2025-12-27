079e49f3947ebd58c34caa612d9cd9d58d6b8b6f
Aleksandr Dyakonov <evosun9@gmail.com>
2025-12-28 00:57:09 +0300

    draft

--- src/lib/util/type/unicode.ts
+++
@@ -1,6 +0,0 @@
-export const split = (string: string) => {
-  return Array.from(
-    new Intl.Segmenter(undefined, { granularity: 'grapheme' }).segment(string),
-    ({ segment }) => segment,
-  )
-}
