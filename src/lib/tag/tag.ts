c6075bf131f669a96fc2b01ae7bbda36f57c17cc
Aleksandr Dyakonov <evosun9@gmail.com>
2026-02-07 11:05:03 +0300

    docs: add inline docs for tags

--- src/lib/tag/tag.ts
+++ src/lib/tag/tag.ts
@@ -1,3 +1,6 @@
+/**
+ * Tag for [tagged templates](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates).
+ */
 export interface Tag<
   Args extends readonly unknown[] = readonly unknown[],
   Returned = unknown,
@@ -5,6 +8,10 @@ export interface Tag<
   (consts: TemplateStringsArray, ...args: Args): Returned
 }
 
+/**
+ * Wrap a tag for [tagged templates](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates)
+ * to get better type inference.
+ */
 export function tag<
   const Args extends readonly unknown[],
   const Returned,
